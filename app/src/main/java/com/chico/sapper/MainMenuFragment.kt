f46db8a11a9ffd427aae68caa0e0f8c432db2c43
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-31 22:56:20 +0300

    MainMenu created

--- app/src/main/java/com/chico/sapper/MainMenuFragment.kt
+++ app/src/main/java/com/chico/sapper/MainMenuFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
 class MainMenuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -12,5 +14,10 @@ class MainMenuFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
     }
 }
