9556562af512d95e3816268798cb12b7886c9e7a
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-31 22:24:30 +0300

    fragments created

--- app/src/main/java/com/chico/sapper/MainActivity.kt
+++ app/src/main/java/com/chico/sapper/MainActivity.kt
@@ -2,10 +2,40 @@ package com.chico.sapper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val splashScreenFragment = SplashScreenFragment()
+        val mainMenuFragment = MainMenuFragment()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(5000)
+            launchFragment(mainMenuFragment,splashScreenFragment)
+        }
+
+    }
+
+    private fun logIsLaunch(isLaunch: Boolean) {
+        Log.i("TAG", "isLaunch = $isLaunch")
+    }
+
+
+    private fun launchFragment(addFragment: Fragment,remFragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+//            .remove(remFragment)
+            .hide(remFragment)
+            .replace(R.id.fragment_holder, addFragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/chico/sapper/MainMenuFragment.kt
@@ -0,0 +1,16 @@
+package com.chico.sapper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class MainMenuFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+}
