6de7de990bebf9799d5edc0166d5ef9e48c8594e
chicony83 <chicony83@mail.com>
2021-01-10 12:11:02 +0300

    version up to 0.2.4 cleanCode

--- app/src/main/java/com/chico/sapper/MainActivity.kt
+++ app/src/main/java/com/chico/sapper/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //        drawView.setOnTouchListener(listenersGameArea.handleTouch);
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -34,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         val splashScreenFragment = SplashScreenFragment()
         val mainMenuFragment = MainMenuFragment()
 
-        isMenuStarting = intent.getBooleanExtra("IS_MENU_STARTING", false)
+        isMenuStarting = intent.getBooleanExtra("IS_MENU_STARTING", true)
 
         if (isMenuStarting) {
             startMenuFragment(mainMenuFragment)
