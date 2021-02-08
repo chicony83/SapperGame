d8c6fb38a71ffea0fa4b41f2104cb890a33491ab
chicony83 <chicony83@mail.com>
2021-02-08 14:53:25 +0300

    title HighScoreFragment.kt depends on the pressed button

--- app/src/main/java/com/chico/sapper/fragments/HighScoreFragment.kt
+++ app/src/main/java/com/chico/sapper/fragments/HighScoreFragment.kt
@@ -1,25 +1,28 @@
 package com.chico.sapper.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.chico.sapper.R
 import com.chico.sapper.dto.enums.BundleStringsNames
 import com.chico.sapper.dto.enums.FragmentsButtonNames
+import com.chico.sapper.dto.enums.HighScoreState
 import com.chico.sapper.interfaces.CallBackInterface
-import com.chico.sapper.viewModel.MyViewModel
 
 class HighScoreFragment : Fragment(), View.OnClickListener {
     private var callBackInterface: CallBackInterface? = null
 
-    private lateinit var viewModelProvider: MyViewModel
+    private lateinit var bottomText: TextView
+    private lateinit var highScoreToHighScoreMenuButton: Button
+    private lateinit var result: String
 
-    private lateinit var highScoreToHighScoreMenuButton:Button
-
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,22 +30,38 @@ class HighScoreFragment : Fragment(), View.OnClickListener {
 
         val rootView = inflater.inflate(R.layout.fragment_high_score, container, false)
 
-        highScoreToHighScoreMenuButton = rootView.findViewById(R.id.highScore_to_highScoreMenu_button)
+        highScoreToHighScoreMenuButton =
+            rootView.findViewById(R.id.highScore_to_highScoreMenu_button)
+        bottomText = rootView.findViewById(R.id.bottomText)
 
         highScoreToHighScoreMenuButton.setOnClickListener(this)
 
         val bundle = arguments
-        val result = bundle?.getString(BundleStringsNames.HIGH_SCORE_STATE.toString())
-//        Log.i("TAG", "result = $result")
+        result = bundle?.getString(BundleStringsNames.HIGH_SCORE_STATE.toString()).toString()
+        Log.i("TAG", "result = $result")
 
-        bundle?.clear()
 
+//        bundle?.clear()
 
         return rootView
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        when (result) {
+            HighScoreState.VERY_EASY.toString() -> setTextOnTextView(getString(R.string.buttonVeryEasy_text))
+            HighScoreState.EASY.toString() -> setTextOnTextView(getString(R.string.buttonEasy_text))
+            HighScoreState.NORMAL.toString() -> setTextOnTextView(getString(R.string.buttonNormal_text))
+            HighScoreState.HARD.toString() -> setTextOnTextView(getString(R.string.buttonHard_text))
+        }
+    }
+
+//    @SuppressLint("ResourceType")
+    private fun setTextOnTextView(buttonText: String) {
+//        Log.i("TAG", "text = $buttonText")
+        bottomText.text = buttonText
     }
 
 
@@ -51,13 +70,13 @@ class HighScoreFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v){
+        when (v) {
             highScoreToHighScoreMenuButton -> {
                 callBack(FragmentsButtonNames.HIGH_SCORE_MENU)
-
             }
         }
     }
+
     private fun callBack(buttonName: FragmentsButtonNames) {
         callBackInterface?.callBackFunction(buttonName)
     }
